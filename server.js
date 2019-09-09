67b3934b014c142eb75cafd7a8c3ac9bfc0c358a
Desiree Morris <desiquinncompany@gmail.com>
2019-09-09 18:24:17 -0400

    hit mvp and finsihed two stretch goals

--- server.js
+++ server.js
@@ -6,4 +6,80 @@ const server = express();
 
 server.use(express.json());
 
+
+server.get('/api/accounts', (req, res) => {
+
+    db('accounts')
+        .then( accounts => {
+            res.json(accounts);
+        })
+        .catch( err => {
+            console.log(err)
+            res.status(500).json({Error: "There was an error"})
+        });
+});
+
+server.post('/api/accounts', (req, res) => {
+    const newAccount = req.body;
+
+    db('accounts')
+        .insert(newAccount)
+        .then(([id]) => {
+            db('accounts')
+                .where({id})
+                .then( account => {
+                res.json(account);
+                })
+        })
+        .catch( err => {
+            console.log(err)
+            res.status(500).json({Error: "There was an error"})
+        });
+});
+
+server.get('/api/accounts/:id', (req, res) => {
+    const { id } = req.params;
+
+    db('accounts')
+        .where({ id })
+        .then( ([account]) => {
+            res.json(account);
+        })
+        .catch( err => {
+            console.log(err)
+            res.status(500).json({Error: "There was an error"})
+        });
+})
+
+server.put('/api/accounts/:id', (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+
+    db('accounts')
+        .where({ id })
+        .update(changes)
+        .then((count) => {
+            res.status(200).json({message: `successfully updated ${count} accounts `})
+        })
+        .catch( err => {
+            console.log(err)
+            res.status(500).json({Error: "There was an error"})
+        });
+});
+
+server.delete('/api/accounts/:id', (req, res) => {
+    const { id } = req.params
+    db('accounts')
+        .where({id})
+        .delete()
+        .then((count) => {
+            res.status(200).json({message: `successfully deleted ${count} accounts `})
+        })
+        .catch( err => {
+            console.log(err)
+            res.status(500).json({Error: "There was an error"})
+        });
+});
+
+
 module.exports = server;
